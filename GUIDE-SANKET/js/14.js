5694cba9a93ec45773ffd23f00a13009ab46a85f
SECRET_CODER <71835543+SecretCoder404@users.noreply.github.com>
2021-01-29 09:51:20 +0530

    last commit

--- GUIDE-SANKET/js/14.js
+++
@@ -1,8 +0,0 @@
-(this.$WP = this.$WP || []).push(["ylqI3R", (e, t) => { "use strict"; var i, n, o, s = () => n.title,
-            u = e => { i(() => { document.title = e }, [e]) },
-            d = () => o,
-            l = ({ image: e, video: t }) => {}; return [() => { n = { title: void 0 }, o = { image: void 0, video: void 0 }, e({ useDocumentTitle: u, usePageContentAnnotations: l, usePublishedDocumentTitle: s, usePublishedPageContentAnnotations: d }) },
-            [e => i = e.useEffect]
-        ] },
-    ["cDcdfi"]
-]);
